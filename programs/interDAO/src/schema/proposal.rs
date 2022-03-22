b0177674249931647df3e34a6022051f0bb7d3b8
tuphan-dn <tuphan@descartes.network>
2022-03-23 05:54:20 +0700

    init dao & proposal

--- programs/interDAO/src/schema/proposal.rs
+++ programs/interDAO/src/schema/proposal.rs
@@ -5,9 +5,10 @@ use anchor_lang::prelude::*;
 pub struct Proposal {
   pub creator: Pubkey,
   pub dao: Pubkey,
+  pub data_len: u64,
   pub data: Vec<u8>,
 }
 
 impl Proposal {
-  pub const HEADER_LEN: usize = DISCRIMINATOR_SIZE + PUBKEY_SIZE + PUBKEY_SIZE; // And a variant data len
+  pub const HEADER_LEN: usize = DISCRIMINATOR_SIZE + PUBKEY_SIZE + PUBKEY_SIZE + U64_SIZE; // And a variant data len
 }
