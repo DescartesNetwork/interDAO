5ad4f59641cbcec2912574e713f52b8ccfeb805b
tuphan-dn <tuphan@descartes.network>
2022-04-12 18:51:25 +0700

    support discriminators

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,19 @@
+import bs58 from 'bs58'
+import { BorshAccountsCoder } from '@project-serum/anchor'
+
 import { IDL } from '../target/types/inter_dao'
 
 export const DEFAULT_RPC_ENDPOINT = 'https://api.devnet.solana.com'
 export const DEFAULT_INTERDAO_PROGRAM_ID =
   '7BxgcaPHKcowjbjA2LjsMNNUaWB3dPFQ9Zn1nChNcm5C'
 export const DEFAULT_INTERDAO_IDL = IDL
+
+export const DAO_DISCRIMINATOR = bs58.encode(
+  BorshAccountsCoder.accountDiscriminator('dao'),
+)
+export const PROPOSAL_DISCRIMINATOR = bs58.encode(
+  BorshAccountsCoder.accountDiscriminator('proposal'),
+)
+export const RECEIPT_DISCRIMINATOR = bs58.encode(
+  BorshAccountsCoder.accountDiscriminator('receipt'),
+)
